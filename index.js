9bbfa927e6f9241c7127a5f1a5e39b4f57a38d82
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-13 00:23:17 -0400

    Create a route in index.js that renders a registration form

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const User = require('./models/user');
 app.set('view engine', 'ejs');
 app.set('views', 'view');
 
+// This route renders a registration form
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+
 // Set up a first basic route
 app.get('/secret', (req, res) => {
     res.send('This is a secret. You cannot see a secret, unless you are logged in!')
