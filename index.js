25d88e47a13f22ddebe944f62526a56a1ec100a6
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-13 00:10:16 -0400

    Require a User Model into index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const User = require('./models/user');
+
 app.listen(3000, () => {
     console.log('Serving the app on port 3000!')
 })
