a9620a4abdfc3fa05fb232a7fdd611459f7f6777
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-12 23:56:24 -0400

    Create a user Mongoose Schema in models/user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username cannot be blank!']
+    },
+    // Here will be saved a hashed password
+    hashedPw: {
+        type: String,
+        required: [true, 'Password cannot be blank!']
+    }
+});
