72c1204e94b1d942c94f5cff67407c5cbe39c78c
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-13 00:17:51 -0400

    Set up a first basic route in index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const User = require('./models/user');
 app.set('view engine', 'ejs');
 app.set('views', 'view');
 
+// Set up a first basic route
+app.get('/secret', (req, res) => {
+    res.send('This is a secret. You cannot see a secret, unless you are logged in!')
+})
+
 app.listen(3000, () => {
     console.log('Serving the app on port 3000!')
 })
