a8073052e40670136f21b627934cb14a53cb0041
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-13 00:14:05 -0400

    Set up view engine and views in index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const User = require('./models/user');
 
+app.set('view engine', 'ejs');
+app.set('views', 'view');
+
 app.listen(3000, () => {
     console.log('Serving the app on port 3000!')
 })
