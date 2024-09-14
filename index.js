3510f5f760608c9494bb901ca97375165c0c7bc3
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-13 23:37:35 -0400

    Require Mongoose into index.js and add code that handles connection to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const User = require('./models/user');
+
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/loginDemo')
+.then(()=>console.log("Mongo connection is open!"))
+.catch((err)=>{
+    console.log("Something went wrong, unable to connect to Mongo");
+    console.log(err);
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
